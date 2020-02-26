bdf1967ee2ee3d3dcadc484c90c62e6b6482397c
吴威 <664507439@qq.com>
2020-02-26 22:52:16 +0800

    no message

---
+++ src/main/java/com/wuwei/SpringCloudEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.rf.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerQuickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDockerQuickApplication.class, args);
+    }
+
+}
