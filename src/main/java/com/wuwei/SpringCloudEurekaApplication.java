629c3bfdf86f50bc5e1b450f1b739b991cd61440
吴威 <664507439@qq.com>
2020-03-03 23:09:54 +0800

    spring boot admin监控添加

--- src/main/java/com/wuwei/SpringCloudEurekaApplication.java
+++ src/main/java/com/wuwei/SpringCloudEurekaApplication.java
@@ -1,11 +1,13 @@
 package com.wuwei;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableAdminServer
 public class SpringCloudEurekaApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudEurekaApplication.class, args);
